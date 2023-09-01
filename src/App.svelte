e7f78e78f2c7f35631239a231464dcac1dda7557
Brandon Willett <bcwillett1@gmail.com>
2023-09-01 12:34:53 -0400

    add navbar component, mocked buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,43 @@
 <script>
   import Chart from './lib/Chart.svelte'
+  import Navbar from './lib/Navbar.svelte'
+
+  import { onMount } from 'svelte';
+
+  import { createClient } from '@supabase/supabase-js'
+
+  onMount(async () => {
+
+    const supabaseUrl = 'https://ewwccbgjnulfgcvfrsvj.supabase.co';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3d2NjYmdqbnVsZmdjdmZyc3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1ODE2ODUsImV4cCI6MjAwOTE1NzY4NX0.gI3YdNSC5GMkda2D2QPRMvnBdaMOS2ynfFKxis5-WKs';
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email: 'bcwillett1@gmail.com',
+      password: 'test'
+    });
+
+    console.log(data)
+    console.log(error)
+  });
 </script>
 
-<main class="w-screen h-screen flex">
-  <div class="w-2/3 h-full">
-    <Chart title="Hits By Status" groupby="statuscode" bucketby="1d"/>
-  </div>
-  <div class="w-1/3 h-full flex flex-col">
-    <div class="w-full h-1/3">
-      <Chart title="Hits by Device" groupby="device" bucketby=""/>
+<main class="w-screen h-screen flex flex-col">
+  <Navbar/>
+  <div class="w-full h-full flex">
+    <div class="w-2/3 h-full">
+      <Chart title="Hits By Status" groupby="statuscode" bucketby="1d"/>
     </div>
-    <div class="w-full h-1/3">
-      <Chart title="Hits by Browser" groupby="browser" bucketby=""/>
-    </div>
-    <div class="w-full h-1/3">
-      <Chart title="Hits by OS" groupby="os" bucketby=""/>
+    <div class="w-1/3 h-full flex flex-col">
+      <div class="w-full h-1/3">
+        <Chart title="Hits by Device" groupby="device" bucketby=""/>
+      </div>
+      <div class="w-full h-1/3">
+        <Chart title="Hits by Browser" groupby="browser" bucketby=""/>
+      </div>
+      <div class="w-full h-1/3">
+        <Chart title="Hits by OS" groupby="os" bucketby=""/>
+      </div>
     </div>
   </div>
 </main>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="w-full h-14 bg-red-800 text-[#E8DED1] flex items-center px-2">
+  <div class="text-4xl font-['SNES'] pr-4 pt-1 shrink-0">CLOUDY BUT NO RAIN</div>
+  <div class="h-full w-full flex justify-between items-center">
+    <div class="ml-2 p-2 hover:bg-red-900 rounded-md cursor-pointer">Site: willett.io</div>
+    <div class="flex">
+      <div class="ml-2 p-2 hover:bg-red-900 rounded-md cursor-pointer">Docs</div>
+      <div class="ml-2 p-2 hover:bg-red-900 rounded-md cursor-pointer">Support</div>
+      <div class="ml-2 p-2 hover:bg-red-900 rounded-md cursor-pointer">Account</div>
+      <div class="ml-2 p-2 hover:bg-red-900 rounded-md cursor-pointer">Sign Out</div>
+    </div>
+  </div>
+</div>
