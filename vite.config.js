f4cb263564b7dbb204485f5b6c9292721bb45490
Brandon Willett <bcwillett1@gmail.com>
2023-10-13 22:11:08 -0400

    lol why was minification off

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: false,
+    minify: true,
   },
   plugins: [svelte()],
 })
