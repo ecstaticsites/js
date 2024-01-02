5b3da311c6c231b54334533cd45e8eb02281c01c
Brandon Willett <bcwillett1@gmail.com>
2024-01-02 16:53:46 -0500

    rotate supa keys, also move to runtime config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -14,9 +14,8 @@ export default class Supabase {
 
     Supabase.instance = this;
 
-    // TODO, get these outta here, env vars
-    let supabaseUrl = 'https://ewwccbgjnulfgcvfrsvj.supabase.co';
-    let supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3d2NjYmdqbnVsZmdjdmZyc3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1ODE2ODUsImV4cCI6MjAwOTE1NzY4NX0.gI3YdNSC5GMkda2D2QPRMvnBdaMOS2ynfFKxis5-WKs';
+    let supabaseUrl = import.meta.env.SUPABASE_URL;
+    let supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 
     this.client = createClient(supabaseUrl, supabaseKey);
   }
